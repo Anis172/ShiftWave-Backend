37495fee5be75e3391d9d7a9b6926bf1678d58c2
Anis172 <anis02ben@gmail.com>
2026-04-28 07:59:17 +0100

    Update BreakRequest.java
    add the utc timezone to localdatetime paramaters so these paramaters actually fit the localtime for all users around the world and avoid hardcoding a single timezone ,scalability friendly practice

--- src/main/java/com/example/restaurantshifthandler/entity/BreakRequest.java
+++ src/main/java/com/example/restaurantshifthandler/entity/BreakRequest.java
@@ -1,9 +1,8 @@
 package com.example.restaurantshifthandler.entity;
 
-
-
 import com.example.restaurantshifthandler.entity.enums.BreakStatus;
 import com.example.restaurantshifthandler.entity.enums.BreakType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -37,13 +36,16 @@ public class BreakRequest {
     @Column(nullable = false, length = 20)
     private BreakStatus status = BreakStatus.PENDING;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @CreationTimestamp
     @Column(name = "requested_at", updatable = false)
     private LocalDateTime requestedAt;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "start_time")
     private LocalDateTime startTime;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "end_time")
     private LocalDateTime endTime;
 }
