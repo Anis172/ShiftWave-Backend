93699732aa85357e9b527ae213158b042a4295f8
Anis172 <anis02ben@gmail.com>
2026-04-28 10:13:06 +0100

    Update ShiftDTO.java
    serialization of dto to json format so timezone is always both in dto and the entity

--- src/main/java/com/example/restaurantshifthandler/dto/ShiftDTO.java
+++ src/main/java/com/example/restaurantshifthandler/dto/ShiftDTO.java
@@ -1,5 +1,7 @@
 package com.example.restaurantshifthandler.dto;
+
 import com.example.restaurantshifthandler.entity.enums.ShiftStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,16 +21,17 @@ public class ShiftDTO {
     @NotNull(message = "Worker is required")
     private Long workerId;
 
-
     private Long restaurantId;
 
     @NotNull(message = "Role is required")
     private Long roleId;
 
     @NotNull(message = "Scheduled start time is required")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private LocalDateTime scheduledStart;
 
     @NotNull(message = "Scheduled end time is required")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private LocalDateTime scheduledEnd;
 
     private ShiftStatus status;
