e44410db79cca708836de0d39283b92601569412
Anis172 <anis02ben@gmail.com>
2026-04-28 10:52:14 +0100

    Update BreakRequestDTO.java
    adding utc timezone to the dto so no conflics happens and the globalization works perfectly while protecting data and respeting clean code architecture as much as possible

--- src/main/java/com/example/restaurantshifthandler/dto/BreakRequestDTO.java
+++ src/main/java/com/example/restaurantshifthandler/dto/BreakRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.restaurantshifthandler.dto;
 
 import com.example.restaurantshifthandler.entity.enums.BreakStatus;
 import com.example.restaurantshifthandler.entity.enums.BreakType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,9 +32,12 @@ public class BreakRequestDTO {
 
     private BreakStatus status;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private LocalDateTime requestedAt;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private LocalDateTime startTime;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private LocalDateTime endTime;
 }
