960cc18ca29ea5f9c8e591b1f3ccbe421e289626
Anis172 <anis02ben@gmail.com>
2026-04-28 08:14:15 +0100

    Update Restaurant.java
    add a missing import

--- src/main/java/com/example/restaurantshifthandler/entity/Restaurant.java
+++ src/main/java/com/example/restaurantshifthandler/entity/Restaurant.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "restaurant")
