6fe67ced20d164a7db623f66e34f23a5646cca19
Anis172 <anis02ben@gmail.com>
2026-04-28 07:56:30 +0100

    Update Shift.java
    add utc to the the localdate so that time will fit the localisation of the user

--- src/main/java/com/example/restaurantshifthandler/entity/Shift.java
+++ src/main/java/com/example/restaurantshifthandler/entity/Shift.java
@@ -1,6 +1,7 @@
 package com.example.restaurantshifthandler.entity;
 
 import com.example.restaurantshifthandler.entity.enums.ShiftStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
@@ -31,9 +32,11 @@ public class Shift {
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "scheduled_start", nullable = false)
     private LocalDateTime scheduledStart;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "scheduled_end", nullable = false)
     private LocalDateTime scheduledEnd;
 
@@ -41,15 +44,15 @@ public class Shift {
     @Column(nullable = false, length = 20)
     private ShiftStatus status = ShiftStatus.SCHEDULED;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "clock_in_time")
     private LocalDateTime clockInTime;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @Column(name = "clock_out_time")
     private LocalDateTime clockOutTime;
-
 
     @OneToMany(mappedBy = "shift", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<BreakRequest> breakRequests;
-
 }
